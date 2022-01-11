506bccb1fb061fcab39d9a4a04ae913883d989cd
Ketlyn Milleny Castro dos Santos <Ketlyncastro2004@gmail.com>
2022-01-11 18:40:14 -0300

    Responsive layout

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import React from 'react'
 
 import First from './components/basics/First'
@@ -8,32 +9,30 @@ import Card from './components/layout/Card'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default _ =>
-    <div id="app">
+    <div className="app">
         <h1>Fundamentos React (Arrow)</h1>
         {/* <Card title="Exemplo de Card"/> */}
 
-        <Card title="#04 - Desafio valor aleatório">
-            <Random min={1} max={31}></Random>
-        </Card>
+        <div className="cards">
+            <Card title="#04 - Desafio valor aleatório">
+                <Random min={1} max={31}></Random>
+            </Card>
 
-        <Card title="#03 - Fragmento">
-            <Fragment></Fragment>
-        </Card>
+            <Card title="#03 - Fragmento">
+                <Fragment></Fragment>
+            </Card>
 
-        <Card title="#02 - Com Parâmetro">
-            <ComParametro
-                title="Situação do Aluno"
-                name="Daniel Santos"
-                note={9.4} />
-        </Card>
+            <Card title="#02 - Com Parâmetro">
+                <ComParametro
+                    title="Situação do Aluno"
+                    name="Daniel Santos"
+                    note={9.4} />
+            </Card>
 
-        <Card title="#01 - Primeiro Componente">
-            <First></First>
-        </Card>
-
-        
-        
-        
+            <Card title="#01 - Primeiro Componente">
+                <First></First>
+            </Card>
+        </div>
     </div>
 
 //função mais reduzida
