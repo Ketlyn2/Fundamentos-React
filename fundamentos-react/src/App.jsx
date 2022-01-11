2fe3a03c9120e472e67b4543bb608b36746e2e44
Ketlyn Milleny Castro dos Santos <Ketlyncastro2004@gmail.com>
2022-01-11 18:50:53 -0300

    cards with different colors

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -14,22 +14,22 @@ export default _ =>
         {/* <Card title="Exemplo de Card"/> */}
 
         <div className="cards">
-            <Card title="#04 - Desafio valor aleatório">
+            <Card title="#04 - Desafio valor aleatório" color="#9B72AA">
                 <Random min={1} max={31}></Random>
             </Card>
 
-            <Card title="#03 - Fragmento">
+            <Card title="#03 - Fragmento" color="#57837B">
                 <Fragment></Fragment>
             </Card>
 
-            <Card title="#02 - Com Parâmetro">
+            <Card title="#02 - Com Parâmetro" color="#DF711B">
                 <ComParametro
                     title="Situação do Aluno"
                     name="Daniel Santos"
                     note={9.4} />
             </Card>
 
-            <Card title="#01 - Primeiro Componente">
+            <Card title="#01 - Primeiro Componente" color="#B85C38">
                 <First></First>
             </Card>
         </div>
--- fundamentos-react/src/components/layout/Card.jsx
+++ fundamentos-react/src/components/layout/Card.jsx
@@ -4,9 +4,18 @@ import React from 'react'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (props) => {
+
+    const cardStyle = {
+        borderColor: props.color || '#C84B31'
+    }
+
+    const titleStyle = {
+        backgroundColor: props.color || '#C84B31'
+    }
+
     return (
-        <div className="card">
-            <div className="title">{ props.title }</div>
+        <div className="card" style={cardStyle}>
+            <div className="title" style={titleStyle}>{ props.title }</div>
             <div className="content">{props.children}</div>
         </div>
     )
